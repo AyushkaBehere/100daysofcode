a13e1b976a480fe88c0ed8edd5a108591f74c17b
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-03-21 22:46:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (61.43%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean isPowerOfFour(int n) {
+        if (n == 1) {
+            return true; 
+        }
+        if (n % 4 != 0 || n == 0){
+            return false;
+        }
+        return isPowerOfFour(n/4);
+    }
+    public static void main (String args[]) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        System.out.println(isPowerOfFour(n));
+    }
+}
