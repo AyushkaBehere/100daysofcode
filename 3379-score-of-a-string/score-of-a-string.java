6b6fde6175d3302fbd4660e0e358e98b8c0c0b93
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-05-07 18:42:51 +0530

    Time: 1 ms (99.92%) | Memory: 42 MB (44.22%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -1,9 +1,36 @@
-class Solution {
-    public int scoreOfString(String s) {
-        int sum = 0;
-        for (int i = 0; i < s.length() - 1; i++) {
-            sum += Math.abs((s.charAt(i) - '0') - (s.charAt(i + 1) - '0'));
-        }
-        return sum;
+ class Solution {
+   public static int scoreOfString(String s) {
+    int sum = 0;
+    for(int i = 0; i<s.length()-1; i++){
+      sum += Math.abs( s.charAt(i)- s.charAt(i+1) );
     }
-}
+    return sum;
+   }
+ }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int scoreOfString(String s) {
+//         int sum = 0;
+//         for (int i = 0; i < s.length() - 1; i++) {
+//             sum += Math.abs((s.charAt(i) - '0') - (s.charAt(i + 1) - '0'));
+//         }
+//         return sum;
+//     }
+// }
