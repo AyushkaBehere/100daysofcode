5cc784759c64a0a249f7654f0c1171da524a1d28
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-06-10 21:26:57 +0530

    Time: 2079 ms (58.35%) | Memory: 40.2 MB (45.08%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,21 +1,50 @@
 class Solution {
     public static boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for (int i = 1; i<= num/2; i++) {
-            if (num % i == 0) {
-                sum = sum + i;
-            }
-        }
-        if (sum == num) {
-            return true;
-        }
-        return false;
+      int sum = 0; 
+      for(int i = 1; i<num; i++){
+        if( num % i == 0 ){
+            sum = sum+i;            
+        }     
     }
+       if( sum == num) {
+          return true;
+        }
 
-    public static void main (String args[]) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("enter your no. : ");
-        int a = sc.nextInt();
-        System.out.println(checkPerfectNumber(a));
+        return false;    
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //int sum = 0;
+    //     for (int i = 1; i<= num/2; i++) {
+    //         if (num % i == 0) {
+    //             sum = sum + i;
+    //         }
+    //     }
+    //     if (sum == num) {
+    //         return true;
+    //     }
+    //     return false;
+    // }
+
+    // public static void main (String args[]) {
+    //     Scanner sc = new Scanner (System.in);
+    //     System.out.println("enter your no. : ");
+    //     int a = sc.nextInt();
+    //     System.out.println(checkPerfectNumber(a));
+    // }
