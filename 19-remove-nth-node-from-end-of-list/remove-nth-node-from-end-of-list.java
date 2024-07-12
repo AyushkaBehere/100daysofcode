cb7f1a14d355d443b9bc439e9ba61bebd9f9b9d4
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-07-12 10:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (32.54%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -9,36 +9,32 @@
  * }
  */
 class Solution {
-    private static ListNode reverseList(ListNode head) {
-        ListNode prev = null, curr = head, temp = null;
-        while(curr != null) {
-            temp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        return prev;
-    }
+     public ListNode removeNthFromEnd(ListNode head, int n) {
+      if(head.next == null) {
+        return null;
+      }
 
-    public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head.next == null)
-            return null;
-        ListNode newHead = reverseList(head), prev = null, curr = newHead;
-        while(--n > 0) {
-            prev = curr;
-            curr = curr.next;
-        }
-        if(prev == null) {
-            //the problem is asking to delete the last node itself
-            prev = curr;
-            curr = curr.next;
-            prev.next = null;
-            head = reverseList(curr);
-        } else {
-            prev.next = curr.next;
-            curr.next = null;
-            head = reverseList(newHead);
-        }
-        return head;
+      //size
+      int size = 0;
+      ListNode curr = head;
+      while(curr!= null) {
+        curr = curr.next;
+        size++;
+      }
+
+      if(n==size) {
+        return head.next;
+      }
+
+      int indexToSearch = size - n;
+      ListNode prev = head;
+      int i = 1;
+      while( i < indexToSearch) {
+        prev = prev.next;
+        i++;
+      }
+
+      prev.next = prev.next.next;
+      return head;
     }
 }
