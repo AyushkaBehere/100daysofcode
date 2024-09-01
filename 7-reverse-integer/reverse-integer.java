433203a898e7fd8c47a51a38fdc651e3c042dffa
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-09-01 22:36:55 +0530

    Time: 1 ms (81.79%) | Memory: 40.7 MB (58.75%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,38 +1,38 @@
-// class Solution {
-//     public int reverse(int x) {
-//         int min = Integer.MIN_VALUE;
-//         int max = Integer.MAX_VALUE;       
-//         int sum = 0;
-//         int rev = 0;        
-        
-//         while( x != 0){
-//               sum =  x % 10;
-//               rev = rev*10+sum;
-//               x = x/10;
-//         }
-              
-//         if( rev > max || rev < min ) {
-//             return 0;
-//         } else {
-//             return rev;
-//         }  
-//     }     
-// }
 class Solution {
     public int reverse(int x) {
-        long reverse = 0;
-        int temp;
-
-        while (x != 0) {
-            temp = x % 10;
-            reverse = reverse * 10 + temp;
-            x = x / 10;
-
-            if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
-                return 0;
-            }
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;       
+        int sum = 0;
+        long rev = 0;        
+        
+        while( x != 0){
+              sum =  x % 10;
+              rev = rev*10+sum;
+              x = x/10;
         }
-
-        return (int)reverse;
-    }
+              
+        if( rev > max || rev < min ) {
+            return 0;
+        } else {
+            return (int)rev;
+        }  
+    }     
 }
+// class Solution {
+//     public int reverse(int x) {
+//         long reverse = 0;
+//         int temp;
+
+//         while (x != 0) {
+//             temp = x % 10;
+//             reverse = reverse * 10 + temp;
+//             x = x / 10;
+
+//             if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+//                 return 0;
+//             }
+//         }
+
+//         return (int)reverse;
+//     }
+// }
