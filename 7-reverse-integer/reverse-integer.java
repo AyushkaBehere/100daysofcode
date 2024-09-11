b2a2e38a66b77b1976979fbff68d979ebf46f89d
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-09-11 23:19:23 +0530

    Time: 1 ms (83.01%) | Memory: 41 MB (25.76%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,38 +1,17 @@
 class Solution {
-    public int reverse(int x) {
-        int min = Integer.MIN_VALUE;
-        int max = Integer.MAX_VALUE;       
-        int sum = 0;
-        long rev = 0;        
-        
-        while( x != 0){
-              sum =  x % 10;
-              rev = rev*10+sum;
-              x = x/10;
+    public static int reverse(int x) {
+        int dig = 0;
+        long sum = 0;       
+        while( x != 0 ){
+            dig = x % 10; 
+            sum = sum *10 + dig;
+            x = x/10;
         }
-              
-        if( rev > max || rev < min ) {
-            return 0;
-        } else {
-            return (int)rev;
-        }  
-    }     
+
+        if( sum > Integer.MAX_VALUE || Integer.MIN_VALUE > sum ) {
+                sum = 0;
+        } 
+       
+        return (int)sum;     
+    }
 }
-// class Solution {
-//     public int reverse(int x) {
-//         long reverse = 0;
-//         int temp;
-
-//         while (x != 0) {
-//             temp = x % 10;
-//             reverse = reverse * 10 + temp;
-//             x = x / 10;
-
-//             if (reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
-//                 return 0;
-//             }
-//         }
-
-//         return (int)reverse;
-//     }
-// }
