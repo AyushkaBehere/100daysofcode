fa6291bab879207e19ebd8a07090e5b6487fc58c
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-09-13 21:41:15 +0530

    Time: 8 ms (51.67%) | Memory: 39.9 MB (92.09%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,11 @@
 class Solution {
-    public static int fib(int n) {
-        if (n==1 || n==0) {
+    public int fib (int n) {
+        if( n == 0 || n == 1) {
             return n;
         }
-        int fib = fib(n-2) + fib(n-1);
 
-        return fib;
+        int fibo = fib(n-1) + fib(n-2);
 
-    }
-
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(fib(n));
+        return fibo;
     }
 }
