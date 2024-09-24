f21e0503029aebf9daba1909af887d55b47816fb
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-09-24 21:39:12 +0530

    Time: 1 ms (67.93%) | Memory: 44.6 MB (63.89%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,31 +1,16 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
-        int j = 0;
-        for(int i = 0; i<nums.length-1; i++){
-             if(nums[i] != nums[i+1]) {
-                nums[j] = nums[i];
-                j++;
-             } 
-        }       
-        nums[j] = nums[nums.length-1];
-        for(int k=0; k<=j; k++){
-            System.out.print(nums[k]);
+        int n = nums.length;
+        if(n == 0) {
+            return 0;
         }
-           return j+1;    
-    }
-
-     
-    public static void main(String args[]) {
-        Scanner sc = new Scanner (System.in);
-        System.out.println("Enter the range of array : ");
-        int range = sc.nextInt();
-
-        int Inputarray[] = new int [range];
-        System.out.println("Enter the array: ");        
-        for(int i=0; i<Inputarray.length; i++) {
-            Inputarray[i] = sc.nextInt();
+        int count = 1;
+        for(int i = 1; i<n; i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[count] = nums[i];
+                count++;
+            } 
         }
+        return count;
     }
-
 }
