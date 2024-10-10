b881c783da3fc8aa8ad6be95d276bbf85b116666
Ayushka Behere <92173376+AyushkaBehere@users.noreply.github.com>
2024-10-10 21:43:45 +0530

    Time: 3 ms (89.79%) | Memory: 44.1 MB (59.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -5,17 +5,19 @@ class Solution {
 
         Arrays.sort(ch);
         Arrays.sort(sh);
+
         if(ch.length != sh.length) {
             return false;
         }
-        for(int i = 0; i<ch.length; i++) {
-            for(int j = i; j<sh.length; j++) {
-                if(ch[i] != sh[j]) {
-                    return false;
-                }
-                break;
-            }
-        }
-        return true;
+         return Arrays.equals(ch, sh);
+        // for(int i = 0; i<ch.length; i++) {
+        //     for(int j = i; j<sh.length; j++) {
+        //         if(ch[i] != sh[j]) {
+        //             return false;
+        //         }
+        //         break;
+        //     }
+        // }
+        // return true;
     }
 }
