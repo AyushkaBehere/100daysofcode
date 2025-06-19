1013569eee3b8bcede03fa0556118c089523d029
Ayushka B <92173376+AyushkaBehere@users.noreply.github.com>
2025-06-19 14:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (28.44%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,19 +1,43 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int n = nums.length;
-        int idx = 0;
-        for(int i = 0; i< n-1; i++) {
-            if(nums[i] == target) {
-                idx = i;
-            }
-            if(nums[i] < target && target <= nums[i+1]) {
-                idx = i+1;
+        int ans = n;
+        int low = 0;
+        int high = n-1;
+        while(low <= high) {
+            int mid = (low+high)/2;
+            if(nums[mid] >= target) {
+                ans = mid;
+                high = mid-1;
+            } else {
+                low = mid + 1;
             }
         }
-        if(target > nums[n-1]) {
-            idx = n;
-        }
-
-        return idx;
+        return ans;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+// int idx = 0;
+//         for(int i = 0; i< n-1; i++) {
+//             if(nums[i] == target) {
+//                 idx = i;
+//             }
+//             if(nums[i] < target && target <= nums[i+1]) {
+//                 idx = i+1;
+//             }
+//         }
+//         if(target > nums[n-1]) {
+//             idx = n;
+//         }
+
+//         return idx;
