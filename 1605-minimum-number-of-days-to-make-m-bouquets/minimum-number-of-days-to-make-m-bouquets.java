2ba6e2673a6de8ad2bd740fccadb383b46b6163b
Ayushka B <92173376+AyushkaBehere@users.noreply.github.com>
2025-07-10 14:46:33 +0530

    Time: 16 ms (93.01%) | Memory: 58.3 MB (55.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,43 +1,45 @@
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        if ((long) m * k > bloomDay.length) {
-            return -1;
+
+    public static boolean possible (int[] bloomDay,int day, int m, int k ) {
+        int count = 0;
+        int noOfB = 0;
+        int n = bloomDay.length;
+        
+        for(int i = 0 ; i< n; i++ ) {
+            if(bloomDay[i] <= day ) {
+                count++;
+            } else {
+                noOfB += (count/k);
+                count=0;
+            }
         }
+        noOfB += (count/k);
+        if( noOfB >= m) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
-        int low = 1, high = (int) 1e9;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (isPossibleBouquets(bloomDay, m, k, mid)) {
-                high = mid;
+    public int minDays(int[] bloomDay, int m, int k) {
+       long val = (long) m * k;
+       int n = bloomDay.length; // Size of the array
+        if (val > n) return -1;
+        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            mini = Math.min(mini, bloomDay[i]);
+            maxi = Math.max(maxi, bloomDay[i]);
+        }
+        int low = mini, high = maxi;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1;
             } else {
                 low = mid + 1;
             }
-        };
-
+        }
         return low;
     }
-    private boolean isPossibleBouquets(int[] bloomDay, int m, int k, int day) {
-        int total = 0;
-
-        for (int i = 0; i < bloomDay.length; i++) {
-            int count = 0;
-            while (i < bloomDay.length && count < k && bloomDay[i] <= day) {
-                count++;
-                i++;
-            }
-
-            if (count == k) {
-                total++;
-                i--;
-            }
-
-            if (total >= m) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
 }
